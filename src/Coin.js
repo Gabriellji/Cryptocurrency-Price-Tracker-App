f37fb9bbb41c6cc4443c797cd79846ee14da2761
Gabriellji <gabriellji@yahoo.com>
2020-11-20 00:35:30 +0300

    fix: fix bug with statement in coin component

--- src/Coin.js
+++ src/Coin.js
@@ -13,10 +13,10 @@ const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap }) =>
                 <div className="coin-data">
                     <p className="coin-price">${price}</p>
                     <p className="coin-volume">${volume.toLocaleString()}</p>
-                    {priceChange < 0 ? (
+                    {priceChange && (priceChange < 0 ? (
                         <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
                     ) : (
-                            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>)}
+                            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>))}
                     <p className="coin-marketcap">
                         Mkt Cap: ${marketcap.toLocaleString()}
                     </p>
